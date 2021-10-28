14de4aa2e0d4e164b231b63fe32555938755a858
Zhengyang Wang <stephen.wang.19@outlook.com>
2021-10-28 10:15:59 -0700

    update

--- js/main.js
+++ js/main.js
@@ -13,56 +13,59 @@ async function geojsonFetch() {
     earthquakes = await response.json();
     response = await fetch ('assets/japan.json');
     japan = await response.json(); 
-};
+
+
+
+    table = document.getElementsByTagName("table")[0];
+    let row, cell1, cell2, cell3;
+    for (let i = 0; i < earthquakes.features.length; i++) {
+        row = table.insertRow(-1);
+        cell1 = row.insertCell(0);
+        cell2 = row.insertCell(1);
+        cell3 = row.insertCell(2);
+        cell1.innerHTML = earthquakes.features[i].properties.id;
+        cell2.innerHTML = earthquakes.features[i].properties.id;
+        cell3,innerHTML = new Date(earthquakes.features[i].properties.time).toLocaleDateString("en-US");
+    }
+
+    map.on('load', function loadingData() {
+
+        map.addSource ('earthquakes', {
+            type: 'geojson',
+            data: earthquakes
+        });
+
+        map.addLayer({
+            'id':'earthquakes-layer',
+            'type':'circle',
+            'source':'earthquakes',
+            'paint':{
+                'circle-radius': 8,
+                'circle-stroke-width': 2,
+                'circle-color': 'red',
+                'circle-stroke-color':'white'
+            }
+        })
+
+
+        map.addSource('japan', {
+            type: 'geojson',
+            data: japan
+        });
+
+        map.addLayer({
+            'id': 'japan-layer',
+            'type':'fill',
+            'source': 'japan',
+            'paint':{
+                'fill-color': '#0080ff',
+                'fill-opacity': 0.5
+            }
+        });
+
+    })
+
+}
 
 geojsonFetch();
 
-map.on('load', function loadingData() {
-
-    map.addSource ('earthquakes', {
-        type: 'geojson',
-        data: earthquakes
-    });
-
-    map.addLayer({
-        'id':'earthquakes-layer',
-        'type':'circle',
-        'source':'earthquakes',
-        'paint':{
-            'circle-radius': 8,
-            'circle-stroke-width': 2,
-            'circle-color': 'red',
-            'circle-stroke-color':'white'
-        }
-    })
-
-
-    map.addSource('japan', {
-        type: 'geojson',
-        data: japan
-    });
-
-    map.addLayer({
-        'id': 'japan-layer',
-        'type':'fill',
-        'source': 'japan',
-        'paint':{
-            'fill-color': '#0080ff',
-            'fill-opacity': 0.5
-        }
-    });
-
-})
-
-
-table = document.getElementsByTagName("table")[0];
-let row, cell1, cell2, cell3;
-for (let i = 0; i < earthquakes.features.length; i++) {
-    row = table.insertRow(-1);
-    cell1 = row.insertCell(0);
-    cell2 = row.insertCell(1);
-    cell3 = row.insertCell(2);
-    cell1.innerHTML = earthquakes.features[i].properties.id;
-    cell2.innerHTML = earthquakes.features[i].properties.id;
-    cell3,innerHTML = new Date(earthquakes.features[i].properties.time).toLocaleDateString("en-US");
-}
